e6660ac15d57d9596967468ac9073f6e37bc73ab
haziq-khurshid <haziq.khurshid@gmail.com>
2022-06-18 18:37:52 +0500

    Remove Nth Node From End of Linke List

--- Linked List/RemoveNthNodeFromEndOfLinkedList.py
+++ Linked List/RemoveNthNodeFromEndOfLinkedList.py
@@ -4,7 +4,7 @@ class Solution:
         for _ in range(n):                       # Take fast pointer to position "n"
             fast = fast.next
         if not fast: return head.next            # If fast pointer is at null, just remove first element and return head
-        while fast.next != None:                 # While fast reaches end, slow will be at previous node of the node to be removed
+        while fast.next != None:                 # When fast reaches end, slow will be at previous node of the node to be removed
             fast = fast.next
             slow = slow.next
         slow.next = slow.next.next               # Skip next node of slow
